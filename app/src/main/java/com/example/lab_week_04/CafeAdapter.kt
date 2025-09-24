b2d80285b155f686c18f228f0e0075d1f6e96ac4
Willbert Budi Lian <willbert.budi@student.umn.ac.id>
2025-09-25 03:24:17 +0700

    Commit No. 4 – Assignment

--- app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
+++ app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
@@ -4,18 +4,23 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
-val TABS_FIXED = listOf(
-    R.string.starbucks_title,
-    R.string.janjijiwa_title,
-    R.string.kopikenangan_title,
-)
-class CafeAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
-    FragmentStateAdapter(fragmentManager, lifecycle) {
+
+
+class CafeAdapter(
+    fragmentManager: FragmentManager,
+    lifecycle: Lifecycle,
+    private val cafeDescriptions: List<String>
+) : FragmentStateAdapter(fragmentManager, lifecycle) {
+
     override fun getItemCount(): Int {
-        return TABS_FIXED.size
+
+        return cafeDescriptions.size
     }
-    override fun createFragment(position: Int): Fragment
-    {
-        return CafeDetailFragment()
+
+    override fun createFragment(position: Int): Fragment {
+
+        val description = cafeDescriptions[position]
+
+        return CafeDetailFragment.newInstance(description)
     }
 }
--- app/src/main/java/com/example/lab_week_04/CafeFragment.kt
+++ app/src/main/java/com/example/lab_week_04/CafeFragment.kt
@@ -1,61 +1,54 @@
 package com.example.lab_week_04
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
+import androidx.fragment.app.Fragment
+import com.example.lab_week_04.databinding.FragmentCafeBinding
 import com.google.android.material.tabs.TabLayoutMediator
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CafeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+private val TABS_FIXED = listOf(
+    R.string.starbucks_title,
+    R.string.janjijiwa_title,
+    R.string.kopikenangan_title,
+)
+
+
+private val CAFE_DESCRIPTIONS = listOf(
+    "Starbucks Corporation is an American multinational chain of coffeehouses and roastery reserves headquartered in Seattle, Washington.",
+    "Janji Jiwa is one of the coffee shop brands that carries a fresh-to-cup concept by serving a selection of classic local Indonesian coffees.",
+    "Kopi Kenangan is an Indonesian coffee company. It is one of the fastest-growing coffee chains in Indonesia."
+)
+
 class CafeFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+
+    private var _binding: FragmentCafeBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cafe, container, false)
+    ): View {
+        _binding = FragmentCafeBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val viewPager = view.findViewById<ViewPager2>(R.id.view_pager)
-        val tabLayout = view.findViewById<TabLayout>(R.id.tab_layout)
-        val adapter = CafeAdapter(childFragmentManager, lifecycle)
-        viewPager.adapter = adapter
-        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+
+
+        val adapter = CafeAdapter(childFragmentManager, lifecycle, CAFE_DESCRIPTIONS)
+        binding.viewPager.adapter = adapter
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
             tab.text = resources.getString(TABS_FIXED[position])
         }.attach()
     }
 
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CafeFragment.
-         */
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CafeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
